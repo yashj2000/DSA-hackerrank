0a316b8dfd0d5903d78d199509a550cc19e8a17d
Yash Janyani <54151714+yashj2000@users.noreply.github.com>
2020-12-30 23:14:46 +0530

    Update game of 2 stacks.py

--- game of 2 stacks.py
+++ game of 2 stacks.py
@@ -1,4 +1,26 @@
-l1=[1,2,3,4]
-for i in l1[::-1]:
-    pass
-print(l1)
+def twostacks(x, a, b):
+    s = c = i = j = 0
+    while i < len(a) and s + a[i] <= x:
+        s += a[i]
+        i += 1
+        c += 1
+    while j < len(b) and i >= 0:
+        s += b[j]
+        j += 1
+        while i > 0 and s > x:
+            i -= 1
+            s -= a[i]
+        if s <= x and c < i + j:
+            c = i + j
+    return c
+
+
+g = int(input())
+for gi in range(g):
+    nmx = input().split()
+    n = int(nmx[0])
+    m = int(nmx[1])
+    x = int(nmx[2])
+    a = list(map(int, input().rstrip().split()))
+    b = list(map(int, input().rstrip().split()))
+    print(twostacks(x, a, b))
